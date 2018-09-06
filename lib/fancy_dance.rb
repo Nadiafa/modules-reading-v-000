a53ffb33cb4674346366ceab423978f0b533fbc2
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-06 17:09:57 +0000

    automatically backed up by learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -2,5 +2,10 @@ module FancyDance
   module InstanceMethods
   end
   module ClassMethods
+    module MetaDancing
+    def metadata
+      "This class produces objects that love to dance."
+    end
+  end
   end
 end 
