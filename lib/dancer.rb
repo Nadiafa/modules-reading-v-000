8f09e529f0aa3c3c319ce2692fda395bbe844354
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-06 16:20:19 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,10 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+
+class Dance 
+  has a #twirl method (FAILED - 1)
+  has a #jump method (FAILED - 2)
+  has a #pirouette method (FAILED - 3)
+  has a #take_a_bow method (FAILED - 4)
+  
+end 
