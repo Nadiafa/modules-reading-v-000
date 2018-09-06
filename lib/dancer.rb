85c48e118418401d971d55090206857c76efea56
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-06 16:21:18 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,9 +2,17 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dance 
-  has a #twirl method (FAILED - 1)
-  has a #jump method (FAILED - 2)
-  has a #pirouette method (FAILED - 3)
-  has a #take_a_bow method (FAILED - 4)
+  def twirl 
+    
+  end 
+  def jump 
+    
+  end 
+  def pirouette 
+    
+  end 
+  def take_a_bow 
+    
+  end 
   
 end 
