f8dc2057bbec1a35eb80047cdf23786e30127479
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-06 16:21:33 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,18 +1,4 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
-class Dance 
-  def twirl 
-    
-  end 
-  def jump 
-    
-  end 
-  def pirouette 
-    
-  end 
-  def take_a_bow 
-    
-  end 
-  
-end 
+ 
